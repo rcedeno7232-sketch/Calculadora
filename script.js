08f4512b8c92a476b9a25b5f3b3058d3707cd04a
rcedeno7232-sketch <rcedeno7232@utm.edu.ec>
2025-11-17 21:53:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Archivo: script.js
+
+const pantalla = document.getElementById("pantalla");
+
+function agregar(valor) {
+  pantalla.value += valor;
+}
+
+function limpiar() {
+  pantalla.value = "";
+}
+
+function calcular() {
+  try {
+    pantalla.value = eval(pantalla.value);
+  } catch {
+    pantalla.value = "Error";
+  }
+}
